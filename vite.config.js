7bd6d7e4f42363d4bd0de3434802c341bd561020
Prateek@9808 <mailprateeksahu@gmail.com>
2025-02-25 20:37:37 +0530

    vsconfig changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
+  base: "/FlyFishingLandingPage/", // Replace with your actual GitHub repo name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Define @ as src
